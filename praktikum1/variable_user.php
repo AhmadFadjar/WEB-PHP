34aa73a91a22dccfa8a0e8f9a5c7e2f9f4584656
Ahmad Fadjar <djarslayer@gmail.com>
2022-06-05 08:02:33 +0700

    Praktikum 1 - Array

---
+++ praktikum1/variable_user.php
@@ -0,0 +1,13 @@
+<?php 
+
+    // mendefinisikan variables
+    $nama = 'Ahmad Fadjar';
+    $umur = 19;
+    $berat = 48;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur.' Tahun';
+    echo '<br/>Berat : '.$berat.' Kg';
+
+    echo "<br/>Hello $nama, gimana kabarmu?";
+?>
